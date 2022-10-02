e9761af4d579e96f5ab774156c474a9b6260f28b
Nguyễn Oanh <103556729+xaviaexo2012@users.noreply.github.com>
2022-10-02 20:42:08 +0700

    Websales-Ecommerce

--- src/main/java/com/websales/entity/Category.java
+++ src/main/java/com/websales/entity/Category.java
@@ -1,4 +1,15 @@
 package com.websales.entity;
 
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Categories")
 public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer id;
+
+    String name;
+
 }
