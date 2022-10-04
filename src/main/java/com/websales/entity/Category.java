ff1c2f520e9e6645f4c35ebf2bea4a0627072499
Nguyễn Oanh <103556729+xaviaexo2012@users.noreply.github.com>
2022-10-04 15:10:00 +0700

    Websales-Ecommerce

--- src/main/java/com/websales/entity/Category.java
+++ src/main/java/com/websales/entity/Category.java
@@ -1,4 +1,54 @@
 package com.websales.entity;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.util.List;
+
+@Entity
+@Table(name = "Categories")
 public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer id;
+
+    String name;
+    @NotBlank(message = "Không được bỏ trống tên loại sản phẩm Tiếng Việt")
+    String nameVN;
+
+
+    @OneToMany(mappedBy = "category", fetch = FetchType.EAGER)
+    List<Product> products;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNameVN() {
+        return nameVN;
+    }
+
+    public void setNameVN(String nameVN) {
+        this.nameVN = nameVN;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+
 }
